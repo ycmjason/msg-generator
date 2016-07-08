8820e81764c90222421d0079a9e6e4a62dd659c5
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-07-08 22:50:06 +0530

    add more cool messages

--- messages/success_troll.js
+++ messages/success_troll.js
@@ -1,5 +1,15 @@
 module.exports = [
   "Successful as hell.",
   "Spectrophotofluorometrically Successful! (whatever it means)",
-  "Oh baby, you made it."
+  "Oh baby, you made it.",
+  "Rename this project to 90DegreeJS because this code works so right",
+  "Smooooothe bruh. It works!",
+  "Knock Knock. Who's there? Mr.Awesome-coder-who-codes-it-right.",
+  "YOU DID IT!!",
+  "*gandalf voice* YOU SHALL PASS!",
+  "Man you're on fire!",
+  "(wo)Man you're on fire! (we want more women in coding)",
+  "That brain tho. Super sexy brain. Code works!",
+  "This code is so fine it makes Kim Kadarshian's butt shy away",
+  "Pure genius"
 ];
