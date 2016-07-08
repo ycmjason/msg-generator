15f8e642fdef15f64e4255ab41632c5779ff0750
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-07-08 22:51:11 +0530

    add more rants

--- messages/failure_offensive.js
+++ messages/failure_offensive.js
@@ -1,5 +1,18 @@
 module.exports = [
   "Failure, just like your life.",
   "Fuck you! Spending hours and hours, but still failing. Useless piece of shit.",
-  "Are you fucking idiot sandwich? It fails again! It's fucking raw!"
-];
+  "Are you fucking idiot sandwich? It fails again! It's fucking raw!",
+  "Your code is so pathetic even console.log needs consoling.",
+  "Fails. Why the fuck did you even start coding?",
+  "Fails. Your dad was right. You're a fucking failure",
+  "I am real tired of your shit, Jimmy.",
+  "That's it, this code is shit. Its a fucking failing shit.",
+  "YAY!!! CODE WORKS!! Just kidding, you're a fucking failure.",
+  "Your failing code is a fucking disgrace to humanity",
+  "Your code's so bad, when it farts people accuse it of global warming.",
+  "Your code's so dumb it thinks menopause is a button on a VCR",
+  "Your code's so dumb, it got fired from a blow job",
+  "Your code's so stupid, it runs a 60 second setTimeout in 24 minutes",
+  "Your code is dumber than Tommen Baratheon. Seriously.",
+  "Your code is Daenerys Targaryen's pussy. Completely fucked up."
+  ];
