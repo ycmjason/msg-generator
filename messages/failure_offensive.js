72c239f2cb07f5e05e50da5d4f6d2cbd16976b70
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-07-09 19:55:01 +0530

    remove unclear failure troll messages

--- messages/failure_offensive.js
+++ messages/failure_offensive.js
@@ -5,14 +5,7 @@ module.exports = [
   "Your code is so pathetic even console.log needs consoling.",
   "Fails. Why the fuck did you even start coding?",
   "Fails. Your dad was right. You're a fucking failure",
-  "I am real tired of your shit, Jimmy.",
   "That's it, this code is shit. Its a fucking failing shit.",
-  "YAY!!! CODE WORKS!! Just kidding, you're a fucking failure.",
-  "Your failing code is a fucking disgrace to humanity",
-  "Your code's so bad, when it farts people accuse it of global warming.",
-  "Your code's so dumb it thinks menopause is a button on a VCR",
-  "Your code's so dumb, it got fired from a blow job",
-  "Your code's so stupid, it runs a 60 second setTimeout in 24 minutes",
-  "Your code is dumber than Tommen Baratheon. Seriously.",
-  "Your code is Daenerys Targaryen's pussy. Completely fucked up."
+  "YAY!!! CODE WORKS!! Just kidding, it does't work.",
+  "Your failing code is a fucking disgrace to humanity"
   ];
